84180da13d5e415a82204b41746a292a66551141
Joao Aslan <contato.joaoaslan@gmail.com>
2025-05-25 22:14:59 -0300

    little improvment in ceasarCypher and extend functions

--- cripto.js
+++ cripto.js
@@ -3,13 +3,15 @@ const msg = "ATACARBASENORTE"
 const key = "FOGO"
 const extended_key = extend(key, msg)
 
+console.log(extended_key)
+
 /* Return the Cesar's alphabet based on caracter especified
    It's the first part of the Vigenere's cypher
 */
 function cesarCypher(char) {
-    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    move = char.charCodeAt(0) - 65
-    let cesar = alphabet.slice(move).concat(alphabet.slice(0, move))
+    const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    const move = char.charCodeAt(0) - 65
+    const cesar = alphabet.slice(move).concat(alphabet.slice(0, move))
     return cesar
 }
 
@@ -17,11 +19,9 @@ function cesarCypher(char) {
 function extend(key, msg) {
     let format_key = ""
     for (let i = 0; i < msg.length; i++) {
-        let index = i
-        if (index >= key.length) {
-            index -= Math.floor(format_key.length / key.length) * key.length
-        }
-        format_key += key[index]
+        const teste = i % key.length
+        console.log(teste)
+        format_key += key[teste]
     }
     return format_key
 }
