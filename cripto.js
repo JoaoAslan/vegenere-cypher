6e9a5d7a38a60803bcaf3057d806d6ba5c773f97
Joao Aslan <contato.joaoaslan@gmail.com>
2025-05-26 23:28:57 -0300

    add vigenereDecrypt function and better validation process

--- cripto.js
+++ cripto.js
@@ -1,10 +1,14 @@
-const msg = "ATACARBASENORTE"
+const msg = "ATA3CAR2BASEN1ORTE"
 const key = "FOGO"
 const code = vigenereCypher(key, msg)
 
 console.log("\nMSG: " + validate(msg))
 console.log("KEY: " + validate(key))
 console.log("CODE: " + code)
+
+console.log("\n\nCODE: "+ validate(code))
+console.log("KEY: "+ validate(key))
+console.log("MSG: "+ vigenereDecrypto(key, code))
 
 // Vigenere's cypher logic
 function vigenereCypher(key, msg) {
@@ -23,6 +27,23 @@ function vigenereCypher(key, msg) {
     return code
 }
 
+// Return MESSAGE based on KEY and CODE (Decrypting Vegenere's cypher)
+function vigenereDecrypt(key, code) {
+    code = validate(code)
+    key = extendKey(validate(key), code.length)
+
+    let msg = ""
+    for (let i = 0; i < key.length; i++) {
+        const ENCODE_A = "A".charCodeAt(0)
+
+        const indexCode = code[i].charCodeAt(0) - ENCODE_A
+        const indexKey = key[i].charCodeAt(0) - ENCODE_A
+        const indexMsg = (indexCode - indexKey + 26) % 26
+        msg += String.fromCharCode(indexMsg + ENCODE_A)
+    }
+    return msg
+}
+
 // Return the extended vesion of key based on the length of message
 function extendKey(key, len) {
     let format_key = ""
@@ -36,13 +57,17 @@ function validate(text) {
     const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     text = text.toUpperCase().trim()
     let validText = ""
+    const deleted = []
     for (let i = 0; i < text.length; i++) { 
         if (alphabet.includes(text[i])) {
             validText += text[i]
+        } else {
+            deleted.push(text[i])
         }
     }
     if (!(validText == text)) {
-        console.log("* Text validation deleted some values!")
+        console.log("\n* Caracteres were deleted: "+ deleted.toString())
+        console.log(" Do not put Special caracteres!")
     }
     return validText
 }
