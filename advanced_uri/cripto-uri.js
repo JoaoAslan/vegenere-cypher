447588eee58a3c636e81943d26dcaef3b546d7e3
Joao Aslan <contato.joaoaslan@gmail.com>
2025-06-01 01:24:54 -0300

    Code refactor, better logic implemetation and new verions

---
+++ advanced_uri/cripto-uri.js
@@ -0,0 +1,73 @@
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+const msg = "Olá, mundo! Vamos codificar?"
+const key = "FOGO"
+
+const encoded = encoderMsg(msg, key)
+const decoded = decryptCode(encoded, key)
+
+console.log("Codificado:", encoded)
+console.log("Decodificado:", decoded)
+
+/* Retorna o código gerado pela mensagem e chave dadas como argumento
+    Utiliza a codificação URI para codificar caracteres especiais (utilizados em URLs)
+*/
+function encoderMsg(msg, key) {
+    let code = "", nonAlphabetCount = 0
+    for (let i = 0; i < msg.length; i++) {
+        if (!(hasInAlphabet(msg[i]))) {
+            code += encodeURIComponent(msg[i])
+            nonAlphabetCount++ // Variavel que controla os saltos da chave
+            continue
+        }
+        const loopKey = key[(i - nonAlphabetCount) % key.length]
+        const letter_case = defineCase(msg[i], loopKey)
+        const indexCode = (letter_case.charIndex + letter_case.keyIndex) % 26
+        code += String.fromCharCode(indexCode + letter_case.base)
+    }
+    return code
+}
+
+/* Retorna a mensagem original com base no código e chave dadas como arguntento
+    Decodifica os caracteres URI para visuzaliação da mensagem original
+*/
+function decryptCode(code, key) {
+    code = decodeURIComponent(code)
+    let msg = "", nonAlphabetCount = 0
+    for (let i = 0; i < code.length; i++) {
+        if (!(hasInAlphabet(code[i]))) {
+            msg += code[i]
+            nonAlphabetCount++ // Variavel que controla os saltos da chave
+            continue
+        }
+        const loopKey = key[(i - nonAlphabetCount) % key.length]
+
+        const letter_case = defineCase(code[i], loopKey)
+        const indexMsg = (letter_case.charIndex - letter_case.keyIndex + 26) % 26
+        msg += String.fromCharCode(indexMsg + letter_case.base)
+    }
+    return msg
+}
+
+/* Define se a letra passada é maiuscula ou minuscula, definindo assim sua posição com base na tabela ASCII
+    https://www.ascii-code.com/
+*/
+function defineCase(char, keyChar) {
+    const LOWER_A = "a".charCodeAt(0)
+    const UPPER_A = "A".charCodeAt(0)
+    let base, caseKey
+    if (char === char.toUpperCase()) {
+        base = UPPER_A
+        caseKey = keyChar.toUpperCase()
+    } else {
+        base = LOWER_A
+        caseKey = keyChar.toLowerCase()
+    }
+    const charIndex = char.charCodeAt(0) - base
+    const keyIndex = caseKey.charCodeAt(0) - base
+    return { base, charIndex, keyIndex }
+}
+
+function hasInAlphabet(char) {
+    return alphabet.includes(char)
+}
